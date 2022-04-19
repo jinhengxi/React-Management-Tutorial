248c3b4bb2e981b9e0647e245da87f5040ac94e6
Hengxi <jinhengxi93@gmail.com>
2022-04-19 16:10:28 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '별이',
+  'birthday' : '001010',
+  'gender' : '여자',
+  'job' : '불가사리'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '징징이',
+  'birthday' : '001111',
+  'gender' : '남자',
+  'job' : '오징어'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '스펀지밥',
+  'birthday' : '001212',
+  'gender' : '남자',
+  'job' : '스펀지'
+}
+
+]
+
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat='logo'/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {
+        customers.map((a)=>{
+          return(
+            <Customer
+              key={a.id}
+              id={a.id}
+              image={a.image}
+              name={a.name}
+              birthday={a.birthday}
+              gender={a.gender}
+              job={a.job}
+            />
+          )
+        })
+      }
     </div>
+    
   );
 }
 
